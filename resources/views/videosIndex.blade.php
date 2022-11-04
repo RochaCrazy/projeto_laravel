d24651a121549ff750af04a91a1502617d6dd48f
Felipe <felipe_vr320@hotmail.com>
2022-11-04 18:00:17 -0300

    update

--- resources/views/videosIndex.blade.php
+++ resources/views/videosIndex.blade.php
@@ -11,13 +11,17 @@
 <div class='index'>
     <a  href="{{route('chupei', substr($image,14,-4))}}">
         <img width="500" height="350" src="{{$image}}" alt="">
-        <p>{{substr($image,14,-4)}}
-            <form action="{{route('chupado', substr($image,14,-4))}}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="btn btn-danger delete-btn">Deletar</button>
-            </form>
-        </p>
+        <h2>{{substr($image,14,-4)}}</h2>
+        <form action="{{route('chupado', substr($image,14,-4))}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                <button type="submit" class="btn btn-danger">Delete</button>
+                <a href="{{route('lambido', substr($image,14,-4))}}">
+                    <button type="button" class="btn btn-warning">Edit</button> 
+                </a>   
+            </div>                 
+        </form>        
     </a>
 </div>
     
